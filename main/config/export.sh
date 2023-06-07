68ab07ae956e2c94d93cf69a7b9ed81f6ed06a96
usyp <69100083+usyp@users.noreply.github.com>
2023-06-08 09:54:33 +1000

    Update export.sh

--- main/config/export.sh
+++ main/config/export.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export PORT=${PORT:-80}
-export USERNAME=${USERNAME:-admin}
-export PASSWORD=${PASSWORD:-admin}
+export USERNAME=${USERNAME:-realadmin}
+export PASSWORD=${PASSWORD:-qAB05KuHiOABqmK}
 export UUID=${UUID:-de04add9-5c68-8bab-950c-08cd5320df18}
 export PATH_VLESS=${PATH_VLESS:-/vless}
 export PATH_VMESS=${PATH_VMESS:-/vmess}
